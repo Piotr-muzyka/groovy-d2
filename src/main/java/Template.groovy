fee1b2e4ee194fb9a57a32c3537544cc33a27f7c
Piotr Muzyka <piotr.muzyka@capgemini.com>
2017-10-04 15:21:21 +0200

    TDD 3rd test

--- src/main/java/Template.groovy
+++ src/main/java/Template.groovy
@@ -5,6 +5,7 @@ class Template {
 
     private static final def EXPRESSION_START  = /\$\{/
     private static final def EXPRESSION_END = /}/
+    private static final def EXPRESSION =  /.*\$\{[^}]+}.*/
 
     private String textWithExpressions
 
@@ -14,11 +15,24 @@ class Template {
     }
 
     String evaluate(Map<String, String> parameters){
+        String result = substituteParameters(parameters)
+        validate(result)
+        return result
+    }
+
+    private String substituteParameters(Map<String, String> parameters){
         String result = textWithExpressions
         for (parameter in parameters){
             result = result.replaceAll(createExpression(parameter.key), parameter.value)
         }
         return result
+    }
+
+    private void validate(String result){
+        //if(result.matches( /.*\$\{[^}]+}.*/)){
+        if (result =~ EXPRESSION) {
+            throw new IllegalArgumentException()
+        }
     }
 
     private String createExpression(String name){
--- src/test/java/TemplateTest.groovy
+++ src/test/java/TemplateTest.groovy
@@ -9,15 +9,26 @@ import static org.junit.Assert.assertEquals
 // TDD - interpolation example
 class TemplateTest {
 
+    private static final String EXPECTED_RESULT = 'My name is Jan Kowalski'
+
+    private Template template = new Template('My name is ${firstName} ${lastName}')
+    // Template is safe (won't be modified by the tests since Junit is creating a separate object for each test)
+
     @Test
     void shouldEvaluateTextWithExpressions(){
-        Template template = new Template('My name is ${firstName} ${lastName}')
+
         String result = template.evaluate([firstName: 'Jan', lastName: 'Kowalski'])
         assertEquals('My name is Jan Kowalski',result)
     }
 
-    @Test
+    @Test (expected = IllegalArgumentException.class) // this test should succeed if we get a IllegalArgumentException
     void shouldThrowException(){
+        new Template('My name is ${firstName} ${lastName}').evaluate([:])
+    }
 
+    @Test
+    void shouldIgnoreExtraParameters(){
+        String result = template.evaluate([firstName: 'Jan', lastName: 'Kowalski', age: '12'])
+        assertEquals('My name is Jan Kowalski',result)
     }
 }

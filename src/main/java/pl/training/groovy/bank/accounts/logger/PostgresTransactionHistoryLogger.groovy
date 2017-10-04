893b4a165c601d249568548ba19a3c24aee78561
Piotr Muzyka <piotr.muzyka@capgemini.com>
2017-10-04 12:14:02 +0200

    DB implementation pt.3

--- src/main/java/pl/training/groovy/bank/accounts/logger/PostgresTransactionHistoryLogger.groovy
+++ src/main/java/pl/training/groovy/bank/accounts/logger/PostgresTransactionHistoryLogger.groovy
@@ -1,9 +1,13 @@
 package pl.training.groovy.bank.accounts.logger
 
+import groovy.sql.Sql
 import groovy.transform.TupleConstructor
+import pl.training.groovy.bank.BankException
 import pl.training.groovy.bank.accounts.Account
 import pl.training.groovy.bank.accounts.Accounts
+import pl.training.groovy.bank.accounts.repository.AccountNotFoundException
 
+import javax.sql.DataSource
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -14,20 +18,33 @@ import java.util.logging.Logger
 class PostgresTransactionHistoryLogger implements Accounts{
 
     private Accounts accounts
-    private Closure<String> currencyFormatter
-    private Logger logger = Logger.getLogger(getClass().name)
+    private Sql sql
+
+    private static final String INSERT_TRANSACTION_INFO = "insert into transactions (id,number,balance,timestamp,type) values(:id,:number,:balance,:timestamp,:type)"
+
+    PostgresTransactionHistoryLogger(Accounts accounts, DataSource dataSource){
+        this.accounts = accounts // this points to the current constructor accounts instance
+        sql = new Sql(dataSource) // we are referencing a method
+    }
 
     Account createAccount() {
-        Account account = accounts.createAccount()
-        logger.log(Level.INFO, "New account created ${account.number}")
-        return account
+        accounts.createAccount()
     }
 
     void deposit(String accountNumber, Long funds) {
-
+        accounts.deposit(accountNumber, funds)
+        process(accountNumber, funds, "DEPOSIT")
     }
 
     void withdraw(String accountNumber, Long funds) {
+        accounts.withdraw(accountNumber, funds)
+        process(accountNumber, funds, "WITHDRAW")
+    }
+
+    private void process(String accountNumber, Long funds, String operationType) {
+
+        Integer date = new Date().getDateTimeString().toInteger()
+        sql.executeInsert(INSERT_TRANSACTION_INFO, [number: accountNumber, balance: funds, timestamp: date, type: operationType])
 
     }
 }

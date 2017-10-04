203483396298beb5ea592e2c646b4d7897ba330d
Piotr Muzyka <piotr.muzyka@capgemini.com>
2017-10-04 09:42:40 +0200

    Fix.

--- src/main/java/pl/training/groovy/bank/accounts/generator/PostgresAccountNumberGenerator.groovy
+++ src/main/java/pl/training/groovy/bank/accounts/generator/PostgresAccountNumberGenerator.groovy
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicLong
 /**
  * Created by PMUZYKA on 2017-10-04.
  */
-class PostgresAccountNumberGenerator extends FakeAccountNumberGenerator{
+class PostgresAccountNumberGenerator extends FakeAccountNumberGenerator {
 
     private static final String SELECT_MAX_ACCOUNT_NUMBER = "select max(number) from accounts"
 
@@ -25,20 +25,17 @@ class PostgresAccountNumberGenerator extends FakeAccountNumberGenerator{
     }
 */
 
-    PostgresAccountNumberGenerator(DataSource dataSource){
+    PostgresAccountNumberGenerator(DataSource dataSource) {
         new Sql(dataSource).eachRow(SELECT_MAX_ACCOUNT_NUMBER, updateCounter) // we are referencing a method
     }
 
 
     private Closure<Void> updateCounter = {
         String lastAccountNumber = it(0) // it is a row , a list with values = on index 0 we have our number inside
-        if (lastAccountNumber){
-            counter = new AtomicLong(lastAccountNumber as Long) // we are casting to Long, AtomicLong - synchronized long
+        if (lastAccountNumber) {
+            counter = new AtomicLong(lastAccountNumber as Long)
+            // we are casting to Long, AtomicLong - synchronized long
         }
 
-    }
-*/
-    String getNext() {
-        return null
     }
 }
